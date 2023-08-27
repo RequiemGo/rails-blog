f95d62391684e84a0f6de0b63226c05e9647e293
Andres Castillo <andres.castillo@meliusid.com>
2023-08-27 16:37:25 -0500

    fix: application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,9 +4,5 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-<<<<<<< HEAD
-import "trix"cambi
-=======
 import "trix"
->>>>>>> 8ef1dda922224ef9eef5dfdf984e91b6809eaefd
 import "@rails/actiontext"
