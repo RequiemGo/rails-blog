a41e4b551d439c818e6e2f16a053c1ae897b9b35
Andres Castillo <andres.castillo@meliusid.com>
2023-08-27 15:56:44 -0500

    ok

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Entry point for the build script in your package.json
+//= require jquery
+//= require jquery-ui
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+import "trix"cambi
+import "@rails/actiontext"
