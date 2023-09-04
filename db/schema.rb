54a2054ad079b23892deb0263a2fc4f300622c73
Andres Castillo <andres.castillo@meliusid.com>
2023-09-03 19:52:18 -0500

    feat: first scaffolds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_27_194559) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_03_223305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_27_194559) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_labels_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title_es"
+    t.string "title_en"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "labels", "posts"
 end
